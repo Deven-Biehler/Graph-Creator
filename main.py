2eaee711e01c8ba3c9a9494bcdee462ec04355e3
Deven Biehler <124630485+Deven-Biehler@users.noreply.github.com>
2023-12-04 15:00:02 -0800

    added delete mode and sidebar

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from vertex import Vertex
 pygame.init()
 
 # Create a Pygame window
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((WIDTH + SIDEBAR_WIDTH, HEIGHT))
 pygame.display.set_caption("Graph Theorist's Sketchpad")
 
 # Vertex information
@@ -26,7 +26,20 @@ drawing_line = False
 # vertex mode: key(Q)
 # edge mode: key(W)
 # move mode: key(E)
+# delete mode: key(D)
 game_mode = "vertex"
+
+# Define colors for the modes
+mode_colors = {
+    "vertex": GREEN,
+    "edge": YELLOW,
+    "move": ORANGE,
+    "delete": RED
+}
+
+# Sidebar information
+sidebar_rect = pygame.Rect(WIDTH, 0, SIDEBAR_WIDTH, HEIGHT)
+sidebar_font = pygame.font.SysFont(None, 20)
 
 # Main game loop
 running = True
@@ -43,8 +56,6 @@ while running:
                 # Add vertex object to collection
                 vertices.append(vertex)
         
-            
-
         if game_mode == "edge":
             if event.type == pygame.MOUSEBUTTONDOWN:
                 # Get position of cursor
@@ -96,6 +107,20 @@ while running:
             elif event.type == pygame.MOUSEBUTTONUP:
                 attached_vertex = None
 
+        elif game_mode == "delete":
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = event.pos
+                for vertex in vertices:
+                    if vertex.is_clicked(x, y):
+                        # Remove the vertex from the collection
+                        vertices.remove(vertex)
+                        # Remove any edges connected to the vertex
+                        edges = [edge for edge in edges if edge.get_start_vertex() != vertex and edge.get_end_vertex() != vertex]
+                        # Clear the selected vertices list if the deleted vertex was selected
+                        if vertex in selected_vertices:
+                            selected_vertices.remove(vertex)
+                        break
+
         if event.type == pygame.MOUSEMOTION:
             if game_mode == "edge":
                 if drawing_line:
@@ -104,13 +129,15 @@ while running:
                 if attached_vertex is not None:
                     attached_vertex.set_position(event.pos)
 
-        elif event.type == pygame.KEYDOWN:
+        elif event.type == pygame.KEYDOWN and not drawing_line:
             if event.key == pygame.K_q:
                 game_mode = "vertex"
             elif event.key == pygame.K_w:
                 game_mode = "edge"
             elif event.key == pygame.K_e:
                 game_mode = "move"
+            elif event.key == pygame.K_d:
+                game_mode = "delete"
 
     # Clear the screen
     screen.fill(WHITE)
@@ -126,6 +153,19 @@ while running:
     # Draw edges
     for edge in edges:
         edge.draw(screen, mouse_pos=pygame.mouse.get_pos())
+
+    # Draw mode indicator
+    mode_indicator_rect = pygame.Rect(10, 10, 100, 30)
+    pygame.draw.rect(screen, mode_colors[game_mode], mode_indicator_rect)
+    mode_indicator_text = pygame.font.SysFont(None, 20).render(game_mode.capitalize(), True, BLACK)
+    screen.blit(mode_indicator_text, (15, 15))
+
+    # Draw sidebar
+    pygame.draw.rect(screen, SIDEBAR_COLOR, sidebar_rect)
+    sidebar_text = sidebar_font.render("Number of Vertices: {}".format(len(vertices)), True, BLACK)
+    screen.blit(sidebar_text, (WIDTH + 10, 10))
+    sidebar_text = sidebar_font.render("Number of Edges: {}".format(len(edges)), True, BLACK)
+    screen.blit(sidebar_text, (WIDTH + 10, 40))
 
     # Update the display
     pygame.display.flip()
